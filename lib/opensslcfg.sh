588af35cc79c13511f8720741575b8bac5bce0a7
Fredrik Thulin <fredrik@thulin.net>
2019-04-02 14:58:49 +0200

    Apparently, issuing is done without subject supplied. Check in 'ici root' instead.

--- lib/opensslcfg.sh
+++ lib/opensslcfg.sh
@@ -3,11 +3,6 @@
 . $ICI_LIB_DIR/lib/config.sh
 . $ICI_CA_DIR/ca.config
 
-if [ "x${ICI_SUBJECT_DN}" = "x" ]; then
-    echo "$0: ICI_SUBJECT_DN not set (try --subject/--subject_dn/--dn/-n)"
-    exit 1
-fi
-
 c_init  > $ICI_CONFIG
 c_req  >> $ICI_CONFIG
 c_dn   >> $ICI_CONFIG
